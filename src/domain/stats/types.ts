1f6a03b192c2f9f0d668ba9acf6f684400aff9c9
zachiagx <zachi@agx.xyz>
2024-06-03 14:32:32 +0800

    chore: types lint

--- src/domain/stats/types.ts
+++ src/domain/stats/types.ts
@@ -1,12 +1,3 @@
-import { ARBITRUM, AVALANCHE } from "config/chains";
-import { BigNumber } from "ethers";
-const key = Symbol(ARBITRUM);
-export type VolumeInfo = {
-  totalVolume: BigNumber;
-  [AVALANCHE]: { totalVolume: BigNumber };
-  [key]: { totalVolume: BigNumber };
-};
-
 export type VolumeStat = {
   swap: string;
   margin: string;
